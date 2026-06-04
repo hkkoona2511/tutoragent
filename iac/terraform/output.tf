c3bde588ea32fb039f6e00a7530052c816b636e4
hkkoona2511 <hkkoona2511@gmail.com>
2026-06-04 14:57:40 +0530

    added Terraform Apply

--- iac/terraform/output.tf
+++ iac/terraform/output.tf
@@ -6,13 +6,13 @@ output "ecr_url" {
 output "ecs_task_definition_json" {
   description = "Full ECS Task Definition formatted as a standard AWS JSON document."
   value = jsonencode({
-    family                  = aws_ecs_task_definition.taskdefone.family
-    networkMode             = aws_ecs_task_definition.taskdefone.network_mode
-    requiresCompatibilities = aws_ecs_task_definition.taskdefone.requires_compatibilities
-    cpu                     = aws_ecs_task_definition.taskdefone.cpu
-    memory                  = aws_ecs_task_definition.taskdefone.memory
-    executionRoleArn        = aws_ecs_task_definition.taskdefone.execution_role_arn
-    taskRoleArn             = aws_ecs_task_definition.taskdefone.task_role_arn
-    containerDefinitions    = jsondecode(aws_ecs_task_definition.taskdefone.container_definitions)
+    family                  = aws_ecs_task_definition.tutoragent.family
+    networkMode             = aws_ecs_task_definition.tutoragent.network_mode
+    requiresCompatibilities = aws_ecs_task_definition.tutoragent.requires_compatibilities
+    cpu                     = aws_ecs_task_definition.tutoragent.cpu
+    memory                  = aws_ecs_task_definition.tutoragent.memory
+    executionRoleArn        = aws_ecs_task_definition.tutoragent.execution_role_arn
+    taskRoleArn             = aws_ecs_task_definition.tutoragent.task_role_arn
+    containerDefinitions    = jsondecode(aws_ecs_task_definition.tutoragent.container_definitions)
   })
 }
